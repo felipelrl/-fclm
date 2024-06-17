4c3111ae5911dfa62afaff68861b80f0553d5ddd
Felipe <flemo@lipigas.cl>
2024-06-17 15:47:19 -0400

    Primer commit

---
+++ src/main/java/com/fclm/FclmApplication.java
@@ -0,0 +1,13 @@
+package com.fclm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FclmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FclmApplication.class, args);
+	}
+
+}

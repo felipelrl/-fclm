8436f5ca433ad25ca99cb8cff8eb45757b442800
Felipe <flemo@lipigas.cl>
2024-06-17 16:09:49 -0400

    Segundo commit

---
+++ src/main/java/com/fclm/Controllers/Test.java
@@ -0,0 +1,15 @@
+package com.fclm.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @GetMapping(value = "/test")
+    protected String genIds(){
+
+        return "Hola";
+
+    }
+}
